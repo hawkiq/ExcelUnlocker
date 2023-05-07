ee437262b3339761de118eb8461453e1c21ef250
hawkiq <osama.sw87@yahoo.com>
2023-05-07 12:22:31 +0300

    Add project files.

---
+++ ExcelDecrypter/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ExcelDecrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.unlockBtnProcess = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.filelbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // unlockBtnProcess
+            // 
+            this.unlockBtnProcess.BackColor = System.Drawing.Color.LightGreen;
+            this.unlockBtnProcess.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.unlockBtnProcess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.unlockBtnProcess.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unlockBtnProcess.ForeColor = System.Drawing.Color.ForestGreen;
+            this.unlockBtnProcess.Location = new System.Drawing.Point(172, 12);
+            this.unlockBtnProcess.Name = "unlockBtnProcess";
+            this.unlockBtnProcess.Size = new System.Drawing.Size(146, 36);
+            this.unlockBtnProcess.TabIndex = 0;
+            this.unlockBtnProcess.Text = "Unlock";
+            this.unlockBtnProcess.UseVisualStyleBackColor = false;
+            this.unlockBtnProcess.Click += new System.EventHandler(this.UnlockBtnProcess_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "xlsm";
+            this.openFileDialog.Filter = "Excel files (*.xls;*.xlsx;*.xlsm)|*.xls;*.xlsx;*.xlsm|All files (*.*)|*.*";
+            this.openFileDialog.Title = "Please Select Excel file";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.openFileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openFileBtn.Location = new System.Drawing.Point(12, 12);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(101, 36);
+            this.openFileBtn.TabIndex = 1;
+            this.openFileBtn.Text = "Open Excel file";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // filelbl
+            // 
+            this.filelbl.Location = new System.Drawing.Point(9, 51);
+            this.filelbl.Name = "filelbl";
+            this.filelbl.Size = new System.Drawing.Size(309, 23);
+            this.filelbl.TabIndex = 2;
+            this.filelbl.Text = "Please Select file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 75);
+            this.Controls.Add(this.filelbl);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.unlockBtnProcess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excel Unlocker by hawkiq";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button unlockBtnProcess;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.Label filelbl;
+    }
+}
+
---
+++ ExcelDecrypter/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+
+
+namespace ExcelDecrypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filelbl.Text = openFileDialog.FileName;
+            }
+                
+        }
+
+        private void UnlockBtnProcess_Click(object sender, EventArgs e)
+        {
+            string filePath = openFileDialog.FileName;
+            Console.WriteLine("Selected file: " + filePath);
+            string extension = Path.GetExtension(filePath);
+            if (extension == ".xls" || extension == ".xlsx" || extension == ".xlsm")
+            {
+                // Create a copy of the original file with a .zip extension in the temporary directory
+                string tempZipFile = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(filePath) + ".zip");
+                Console.WriteLine("Temporary ZIP file: " + tempZipFile);
+                File.Copy(filePath, tempZipFile);
+
+                // Extract the contents of the ZIP file to a temporary directory
+                string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                Console.WriteLine("Temporary directory: " + tempDir);
+                ZipFile.ExtractToDirectory(tempZipFile, tempDir);
+
+                // Navigate to specific folder inside extracted contents
+                string targetDir = Path.Combine(tempDir, "xl", "worksheets");
+                string[] files = Directory.GetFiles(targetDir, "sheet*");
+                Console.WriteLine("Found {0} files starting with 'sheet':", files.Length);
+                // Do processing on files in target folder
+                foreach (string file in files)
+                {
+                    Console.WriteLine("  " + file);
+
+                    XDocument doc = XDocument.Load(file);
+
+                    // Remove any sheetProtection elements from the document
+                    // Get all sheetProtection elements
+                    XNamespace ns = "http://schemas.openxmlformats.org/spreadsheetml/2006/main";
+                    IEnumerable<XElement> sheetProtections = doc.Descendants(ns+ "sheetProtection");
+
+                    // Print number of sheetProtection elements found
+                    Console.WriteLine("Found {0} sheetProtection elements in file {1}", sheetProtections.Count(), file);
+
+                    // Remove all sheetProtection elements
+                    sheetProtections.Remove();
+
+
+                    // Save the modified XDocument object back to the file
+                    doc.Save(file);
+                }
+
+                // Compress the modified files into a ZIP file
+                string zipFileName = Path.Combine(Path.GetTempPath(), "edited_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".zip");
+                Console.WriteLine("Compressed ZIP file: " + zipFileName);
+
+                ZipFile.CreateFromDirectory(tempDir, zipFileName, CompressionLevel.Optimal, false);
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Excel Macro-Enabled Workbook (*.xlsm)|*.xlsm";
+                saveFileDialog.FileName = Path.GetFileNameWithoutExtension(filePath) + "_hawkiq_unlocked_" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Copy the ZIP file to the selected location with a .xlsm extension
+                    string xlsmFileName = Path.ChangeExtension(saveFileDialog.FileName, ".xlsm");
+                    Console.WriteLine("Selected XLSM file: " + xlsmFileName);
+                    File.Copy(zipFileName, xlsmFileName);
+
+                    // Open the folder where the file was saved 
+                    string folderPath = Path.GetDirectoryName(xlsmFileName);
+                    Process.Start(folderPath);
+                }
+                // Clean up temporary files and directories
+                File.Delete(tempZipFile);
+                File.Delete(zipFileName);
+                Directory.Delete(tempDir, true);
+                Console.WriteLine("Temporary files and directories deleted.");
+            }
+            else
+            {
+                MessageBox.Show("Selected file is not a Excel file.");
+            }
+        }
+    }
+}
